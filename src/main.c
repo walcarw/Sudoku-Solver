f0fa049076a67e71ce30030ba5449910749d24de
WilliamWalca <132449932+WilliamWalca@users.noreply.github.com>
2024-07-17 13:57:17 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 #include "sudoku.h"
 
-
-int main(){
-    printf("Loading the Sudoku grid.\n");
-    sudokuGrid *grid = loadGrid("example.txt");
+/**
+ * @brief Main function to load a Sudoku grid from a file, solve it, and display the results.
+ *
+ * @param argc Argument count.
+ * @param argv Argument vector containing command line arguments.
+ * @return Exit status.
+ */
+int main(int argc, char *argv[]){
+    if (argc < 2){
+        fprintf(stderr, "Error: Please provide a file as an argument.\n");
+        return 1;
+    }
+        
+    sudokuGrid *grid = loadGrid(argv[1]);
+    printf("Sudoku grid:\n");
     printGrid(grid);
-    printf("Solving\n");
+    printf("Solution:\n");
     backtracking_solve(grid);
     free(grid);
     return 0;
